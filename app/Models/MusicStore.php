b149f87c9245006e271fda91f1e62d4bece92918
KitaevNikita <nikita.kitaev.06@mail.ru>
2021-12-28 09:13:21 +0300

    Создание контроллеров

---
+++ app/Models/MusicStore.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class MusicStore extends Model
+{
+    use HasFactory;
+}
--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MusicController;
+use App\Http\Controllers\CategoryController;
+
+Route::resource('categories', MusicController::class);
 
